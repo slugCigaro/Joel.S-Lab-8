6bb0102877244ca396ecb03027fa92f37999b474
Student <jsitbon@qcc.cuny.edu>
2018-10-26 13:11:52 -0400

    Initial commit

---
+++ Joel.S Lab 8 Exp. 1/Joel.S Lab 8/Experiment 1.cpp
@@ -0,0 +1,13 @@
+//A well-adjusted printing program #2
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "The following items were top sellers" << endl;
+	cout << "during the monthe of June:" << endl;
+	cout << "Computer games" << endl;
+	cout << "Coffee" << endl;
+	cout << "Aspirin" << endl;
+	system("pause");
+	return 0;
+}
---
+++ Joel.S Lab 8 Exp.2/Joel.S Lab 2 Exp.2/this program has a variable.cpp
@@ -0,0 +1,17 @@
+// This program has a variable. Program #3
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+
+	number = 5;
+	cout << "The value of number is " << "number" << endl;
+	cout << "The value of number is " << number << endl;
+
+	number = 7;
+	cout << "Now the value of number is " << number << endl;
+
+	system("pause");
+	return 0;
+}
